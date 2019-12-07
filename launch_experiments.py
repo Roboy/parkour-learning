fbc6278f879dd5e5aaa610f4931296c58628049e
Alexander Koch <alexander@4koch.de>
2019-12-07 21:12:39 +0100

    add script to launch multiple training configs

---
+++ launch_experiments.py
@@ -0,0 +1,93 @@
+"""
+Launches multiple experiment runs and organizes them on the local
+compute resource.
+Processor (CPU and GPU) affinities are all specified, to keep each
+experiment on its own hardware without interference.  Can queue up more
+experiments than fit on the machine, and they will run in order over time.
+
+To understand rules and settings for affinities, try using
+affinity = affinity.make_affinity(..)
+OR
+code = affinity.encode_affinity(..)
+slot_code = affinity.prepend_run_slot(code, slot)
+affinity = affinity.affinity_from_code(slot_code)
+with many different inputs to encode, and see what comes out.
+
+The results will be logged with a folder structure according to the
+variant levels constructed here.
+
+"""
+import multiprocessing
+import GPUtil
+from rlpyt.utils.launching.affinity import encode_affinity, quick_affinity_code
+from rlpyt.utils.launching.exp_launcher import run_experiments
+from rlpyt.utils.launching.variant import make_variants, VariantLevel
+
+# Either manually set the resources for the experiment:
+num_cpus = multiprocessing.cpu_count()
+num_gpus = len(GPUtil.getAvailable())
+affinity_code = encode_affinity(
+    n_cpu_core=num_cpus,
+    n_gpu=num_gpus,
+    # hyperthread_offset=8,  # if auto-detect doesn't work, number of CPU cores
+    # n_socket=1,  # if auto-detect doesn't work, can force (or force to 1)
+    cpu_per_run=8,
+    gpu_per_run=0,  # min(num_gpus, 1),
+    set_affinity=True,  # it can help to restrict workers to individual CPUs
+)
+# Or try an automatic one, but results may vary:
+# affinity_code = quick_affinity_code(n_parallel=None, use_gpu=True)
+
+runs_per_setting = 1
+experiment_title = "pc"
+variant_levels = list()
+
+# Within a variant level, list each combination explicitly.
+# learning_rate = [7e-4, 1e-3]
+# batch_B = [16, 32]
+# values = list(zip(learning_rate, batch_B))
+# dir_names = ["example6_{}lr_{}B".format(*v) for v in values]
+# keys = [("algo", "learning_rate"), ("sampler_kwargs", "batch_B")]
+# variant_levels.append(VariantLevel(keys, values, dir_names))
+
+algos = ["ppo", "sac"]
+values = list(zip(algos))
+dir_names = ["{}".format(*v) for v in values]
+keys = [("algo",), ]
+variant_levels.append(VariantLevel(keys, values, dir_names))
+
+batch_sizes = [256, 512, 1024]
+values = list(zip(batch_sizes))
+dir_names = ["{}".format(*v) for v in values]
+keys = [("algo_kwargs", 'batch_size'), ]
+variant_levels.append(VariantLevel(keys, values, dir_names))
+
+games = [dict(env_kwargs=dict(id="ParkourChallenge-v0"))]
+values = list(zip(games))
+dir_names = ["{}".format(*v) for v in values]
+keys = [("sampler_kwargs",)]
+variant_levels.append(VariantLevel(keys, values, dir_names))
+
+# Between variant levels, make all combinations.
+variants, log_dirs = make_variants(*variant_levels)
+
+variants = [
+    {
+        'algo': 'sac',
+        'sac_kwargs': dict(batch_size=256)
+    },
+    {
+        'algo': 'sac',
+        'sac_kwargs': dict(batch_size=1024)
+    },
+]
+log_dirs = ["pc_" + str(id) for id in range(len(variants))]
+
+run_experiments(
+    script="train.py",
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+)
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ Requires OpenAI gym (and maybe mujoco).  If not installed, move on to next
 example.
 
 """
-
+import sys
 from rlpyt.samplers.serial.sampler import SerialSampler
 from rlpyt.samplers.async_.cpu_sampler import AsyncCpuSampler
 from rlpyt.envs.gym import make as gym_make
@@ -20,12 +20,17 @@ from logger_context import config_logger
 import os
 from rlpyt.utils.launching.affinity import make_affinity
 from rlpyt.envs.gym import GymEnvWrapper, EnvInfoWrapper
-from rlpyt.utils.logging import logger
+from rlpyt.algos.pg.ppo import PPO
+from rlpyt.agents.pg.mujoco import MujocoLstmAgent
 from torch.utils.tensorboard.writer import SummaryWriter
+from vision_models import PiMlpModel
 import gym
 import torch
 import GPUtil
 import multiprocessing
+from rlpyt.models.pg.mujoco_lstm_model import MujocoLstmModel
+from rlpyt.utils.launching.affinity import affinity_from_code
+from rlpyt.utils.launching.variant import load_variant, update_config
 
 
 def make(*args, info_example=None, **kwargs):
@@ -36,55 +41,87 @@ def make(*args, info_example=None, **kwargs):
         gym.make(*args, **kwargs), info_example))
 
 
-def build_and_train(env_id="Hopper-v3", snapshot_file: str=None, run_ID=0, cuda_idx=None):
+def build_and_train(slot_affinity_code='1slt_2cpu_0gpu_1cpr_4hto', log_dir='./data', run_ID=0, snapshot_file: str=None):
+    config = dict(
+        sac_kwargs=dict(learning_rate=7e-4, batch_size=256),
+        ppo_kwargs=dict(),
+        sampler_kwargs=dict(batch_T=5, batch_B=7, env_kwargs=dict(id="ParkourChallenge-v0")),
+        runner_kwargs=dict(n_steps=1e9),
+        snapshot_file=snapshot_file,
+        algo='sac'
+    )
+    affinity = affinity_from_code(slot_affinity_code)
+    try:
+        variant = load_variant(log_dir)
+        config = update_config(config, variant)
+    except FileNotFoundError:
+        pass
     agent_state_dict = optimizer_state_dict = None
-    if snapshot_file is not None:
+    if config['snapshot_file'] is not None:
         snapshot = torch.load(snapshot_file)
         agent_state_dict = snapshot['agent_state_dict']
         optimizer_state_dict = snapshot['optimizer_state_dict']
 
-    num_cpus = multiprocessing.cpu_count()
+    # num_cpus = multiprocessing.cpu_count()
     num_gpus = len(GPUtil.getAvailable())
 
     if num_gpus > 0:
         SamplerClass = AsyncCpuSampler
         RunnerClass = AsyncRlEval
-        affinity = make_affinity(
-            run_slot=0,
-            n_cpu_core=8,  # Use 16 cores across all experiments.
-            n_gpu=1,  # Use 8 gpus across all experiments.
-            gpu_per_run=1,
-            sample_gpu_per_run=0,
-            async_sample=True,
-            optim_sample_share_gpu=False,
-            # hyperthread_offset=16,  # If machine has 24 cores.
-            # n_socket=2,  # Presume CPU socket affinity to lower/upper half GPUs.
-            # gpu_per_run=2,  # How many GPUs to parallelize one run across.
-            # cpu_per_run=1,
-        )
+        affinity['async_sample'] = True
+        # affinity = make_affinity(
+        #     run_slot=0,
+        #     n_cpu_core=8,  # Use 16 cores across all experiments.
+        #     n_gpu=1,  # Use 8 gpus across all experiments.
+        #     gpu_per_run=1,
+        #     sample_gpu_per_run=0,
+        #     async_sample=True,
+        #     optim_sample_share_gpu=False,
+        #     # hyperthread_offset=16,  # If machine has 24 cores.
+        #     # n_socket=2,  # Presume CPU socket affinity to lower/upper half GPUs.
+        #     # gpu_per_run=2,  # How many GPUs to parallelize one run across.
+        #     # cpu_per_run=1,
+        # )
     else:
-        affinity=dict() # ;dict(cuda_idx=cuda_idx),
+        # affinity=dict() # ;dict(cuda_idx=cuda_idx),
         SamplerClass = SerialSampler
         RunnerClass = MinibatchRlEval
 
+    if config['algo'] == 'ppo':
+        AgentClass = MujocoLstmAgent
+        AlgoClass = PPO
+        algo_kwargs = config['ppo_kwargs']
+    elif config['algo'] == 'sac':
+        AgentClass = SacAgent
+        AlgoClass = SAC
+        algo_kwargs = config['sac_kwargs']
+
+
+
     sampler = SamplerClass(
+        **config['sampler_kwargs'],
         EnvCls=make,
-        env_kwargs=dict(id=env_id),
-        eval_env_kwargs=dict(id=env_id),
-        batch_T=5,  # One time-step per sampler iteration.
-        batch_B=7,  # One environment (i.e. sampler Batch dimension).
+        # env_kwargs=dict(id='ParkourChallenge-v0'),
+        eval_env_kwargs=dict(id='ParkourChallenge-v0'),
+        # batch_T=5,  # One time-step per sampler iteration.
+        # batch_B=7,  # One environment (i.e. sampler Batch dimension).
         max_decorrelation_steps=0,
         eval_n_envs=10,
-        eval_max_steps=int(51e3),
+        eval_max_steps=int(1e3),
         eval_max_trajectories=10,
     )
-    algo = SAC(initial_optim_state_dict=optimizer_state_dict)  # Run with defaults.
-    agent = SacAgent(initial_model_state_dict=agent_state_dict)
+    # algo = SAC(initial_optim_state_dict=optimizer_state_dict)  # Run with defaults.
+    # agent = SacAgent(
+    #     initial_model_state_dict=agent_state_dict,
+    #     ModelCls=MujocoLstmModel
+    # )
+    algo = AlgoClass(**algo_kwargs, initial_optim_state_dict=optimizer_state_dict)
+    agent = AgentClass(initial_model_state_dict=agent_state_dict)
     runner = RunnerClass(
+        **config['runner_kwargs'],
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=1e7,
         log_interval_steps=1e5,
         affinity=affinity
     )
@@ -93,16 +130,18 @@ def build_and_train(env_id="Hopper-v3", snapshot_file: str=None, run_ID=0, cuda_
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--env_id', help='environment ID', default='ParkourChallenge-v0')
-    parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
-    parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
-    args = parser.parse_args()
-    build_and_train(
-        env_id=args.env_id,
-        run_ID=args.run_ID,
-        cuda_idx=args.cuda_idx,
-        # snapshot_file='./data/run_5/params.pkl'
-    )
+    build_and_train(*sys.argv[1:])
+    # import argparse
+    #
+    # parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument('--env_id', help='environment ID', default='ParkourChallenge-v0')
+    # parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
+    # parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
+    # args = parser.parse_args()
+    # build_and_train(
+    #     slot_affinity_code=None,
+    #     env_id=args.env_id,
+    #     run_ID=args.run_ID,
+    #     cuda_idx=args.cuda_idx,
+    #     # snapshot_file='./data/run_5/params.pkl'
+    # )
