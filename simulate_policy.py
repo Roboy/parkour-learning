7f7c6668a7541441d30f1ec7866c3ee4bdb89d02
Alexander Koch <alexander@4koch.de>
2019-12-15 19:43:23 +0100

    made number of steps unlimited

--- simulate_policy.py
+++ simulate_policy.py
@@ -14,16 +14,17 @@ def simulate_policy(path_to_params):
     optimizer_state_dict = snapshot['optimizer_state_dict']
 
     agent = SacAgent()
-    # env = gym.make('ParkourChallenge-v0', render=True,)
-    env = gym.make('HopperPyBulletEnv-v0')
-    env.render()
+    env = gym.make('ParkourChallenge-v0', render=True,)
+    # env = gym.make('HopperPyBulletEnv-v0')
+    env.render(mode='human')
     wrapped_env = GymEnvWrapper(env)
     agent.initialize(env_spaces=wrapped_env.spaces)
     agent.load_state_dict(agent_state_dict)
 
     while True:
         obs = env.reset()
-        for step in range(100):
+        done = False
+        while not done:
             action = env.action_space.sample()
             obs = torch.from_numpy(obs)
             action = agent.pi(obs, None, None)[0].detach().numpy()
