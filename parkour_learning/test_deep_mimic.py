7060cb6f0893d0c2a356ecc0d44454458acb91ee
alex <alexander@4koch.de>
2020-02-02 13:59:55 +0100

    reduce acion frequenc

--- parkour_learning/test_deep_mimic.py
+++ parkour_learning/test_deep_mimic.py
@@ -2,14 +2,22 @@ import parkour_learning
 import gym
 import time
 import sys
+
 mocap_file_path = './motions/humanoid3d_jump.txt'
 env = gym.make('HumanoidDeepMimicBulletEnv-v1', render=True)
 while True:
     obs = env.reset()
+    env.reset()
     done = False
+    time.sleep(0.5)
+    step =0
 
     while not done:
+        step += 1
         action = env.action_space.sample()
-        time.sleep(0.01)
+        action *=0
+        time.sleep(0.03)
         obs, reward, done, info = env.step(action)
+        print('reward: ' +str(reward))
         # env.render()
+    print('num steps:' + str(step))
