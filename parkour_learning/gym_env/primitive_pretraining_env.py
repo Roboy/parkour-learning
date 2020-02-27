f27e0c7567698bf82f7b7c53b5a31da94cb2be39
alex <alexander@4koch.de>
2020-02-27 21:23:44 +0100

    fix grads

--- parkour_learning/gym_env/primitive_pretraining_env.py
+++ parkour_learning/gym_env/primitive_pretraining_env.py
@@ -17,7 +17,7 @@ from parkour_learning.gym_env.humanoid import Humanoid
 
 
 class PrimitivePretrainingEnv(gym.Env):
-    mocap_files = ['run.txt', 'jump_and_roll.txt', 'vaulting.txt', 'getup_facedown.txt']
+    mocap_files = ['run.txt', 'jump_and_roll.txt', 'vaulting.txt', 'run.txt']
     mocap_folder = osp.join(osp.dirname(__file__), '../motions/')
 
     def __init__(self, render=False):
