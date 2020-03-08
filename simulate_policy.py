bcbb74d59afa31d49bef64c23a18a6bd7bd24491
alex <alexander@4koch.de>
2020-03-08 19:20:48 +0100

    add timer to achieve fixed loop time

--- simulate_policy.py
+++ simulate_policy.py
@@ -7,10 +7,11 @@ import parkour_learning
 import pybulletgym  # register PyBullet enviroments with open ai gym
 import gym_parkour
 from rlpyt.agents.qpg.sac_agent import SacAgent
-from mcp_sac_agent import MCPSacAgent
+# from mcp_sac_agent import MCPSacAgent
 from rlpyt.envs.gym import GymEnvWrapper
 from rlpyt.utils.buffer import torchify_buffer, buffer_from_example, numpify_buffer
-from mcp_model import PiMCPModel, QofMCPModel, PPOMcpModel
+# from mcp_model import PiMCPModel, QofMCPModel, PPOMcpModel
+from mcp_vision_model import PPOMcpModel
 from rlpyt.agents.pg.mujoco import MujocoLstmAgent, MujocoFfAgent
 
 # from mcp_vision_model import PiMCPModel, QofMCPModel
@@ -22,16 +23,16 @@ def simulate_policy(path_to_params, env_id: str):
     # env = gym.make('HopperPyBulletEnv-v0')
     # env.render()
     # env = GymEnvWrapper(env)
-    agent_kwargs = dict(ModelCls=PiMCPModel, QModelCls=QofMCPModel)
-    agent = MCPSacAgent(**agent_kwargs)
-    # agent = SacAgent(model_kwargs=dict(hidden_sizes=[512,256, 256]), q_model_kwargs=dict(hidden_sizes=[512, 256, 256]))
+    # agent_kwargs = dict(ModelCls=PiMCPModel, QModelCls=QofMCPModel)
+    # agent = SacAgent(**agent_kwargs)
+    agent = SacAgent(model_kwargs=dict(hidden_sizes=[512,256, 256]), q_model_kwargs=dict(hidden_sizes=[512, 256, 256]))
     # agent = MujocoFfAgent(ModelCls=PPOMcpModel)
     agent.initialize(env_spaces=env.spaces)
     agent.load_state_dict(agent_state_dict)
     agent.eval_mode(0)
     obs = env.reset()
     observation = buffer_from_example(obs, 1)
-
+    loop_time = 0.05
     while True:
         observation[0] = env.reset()
         action = buffer_from_example(env.action_space.null_value(), 1)
@@ -41,6 +42,7 @@ def simulate_policy(path_to_params, env_id: str):
         step = 0
         reward_sum = 0
         while not done:
+            loop_start = time.time()
             step += 1
             print(step)
             act_pyt, agent_info = agent.step(obs_pyt, act_pyt, rew_pyt)
@@ -51,7 +53,9 @@ def simulate_policy(path_to_params, env_id: str):
             observation[0] = obs
             # action[0] = action
             rew_pyt[0] = reward
-            time.sleep(0.02)
+            sleep_time = loop_time - (time.time() - loop_start)
+            sleep_time = 0 if (sleep_time < 0) else sleep_time
+            time.sleep(sleep_time)
             env.render(mode='human')
         print('return: ' + str(reward_sum) + '  num_steps: ' + str(step))
 
